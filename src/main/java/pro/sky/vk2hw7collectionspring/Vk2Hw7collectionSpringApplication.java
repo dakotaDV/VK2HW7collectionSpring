66a673b7017f2769306a5ccd4562271056e841d5
Prudnikova <dakoto4ka@yandex.ru>
2023-04-10 16:44:46 +0300

    Initial commit

---
+++ src/main/java/pro/sky/vk2hw7collectionspring/Vk2Hw7collectionSpringApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.vk2hw7collectionspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vk2Hw7collectionSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Vk2Hw7collectionSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/vk2hw7collectionspring/Vk2Hw7collectionSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.vk2hw7collectionspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vk2Hw7collectionSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
